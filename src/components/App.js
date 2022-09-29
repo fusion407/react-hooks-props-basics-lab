a12525478c6ca93b7a288fad413858ad5640ba77
fusion407 <fusion407@gmail.com>
2022-09-29 17:35:47 -0500

    commit tree 7523f722644f09f30067021b3783fc21ed09440e

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,15 @@ import user from "../data/user";
 console.log(user);
 
 function App() {
+  const user_name = user.name;
+  const user_city = user.city;
+  const user_color = user.color;
+  const user_bio = user.bio;
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user_name} city={user_city} color={user_color}/>
+      <About bio={user_bio} github={user.links.github} linkedin={user.links.linkedin}/>
     </div>
   );
 }
